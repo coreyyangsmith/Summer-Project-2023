9f0065d9862f317bfee00040b4329cf4debafdbf
Corey Yang-Smith <yangsmith.corey@hotmail.com>
2023-07-15 11:43:52 -0600

    separate concerns for base and home html

--- main/views.py
+++ main/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import User
 
 # Create your views here.
-def index(request):
-    return HttpResponse("This is the (main) voting page")
+def index(response, id):
+    user = User.objects.get(id=id)
+    return render(response, 'main/display.html', {"user":user})
+
+def home(response):
+    return render(response, 'main/home.html',{})
