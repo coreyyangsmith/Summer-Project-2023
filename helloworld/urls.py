7e8bf326ad691aa0954f7795d298535aa78be0cb
Corey Yang-Smith <yangsmith.corey@hotmail.com>
2023-07-15 12:06:52 -0600

    add results template files

--- helloworld/urls.py
+++ helloworld/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('results/', include("results.urls")),        
     path('', include("main.urls")),
-    path('results/', include("results.urls")),    
+
 ]
--- results/views.py
+++ results/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-def index(request):
-    return HttpResponse("This is the results page")
+def index(response):
+    return render(response, 'results/results.html',{})
