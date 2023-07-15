1c93f99599a3eaa30a64f8cb153166841ebe5fce
Corey Yang-Smith <yangsmith.corey@hotmail.com>
2023-07-15 08:38:58 -0600

    add sqlite3 database to proejct with test model

--- helloworld/urls.py
+++ helloworld/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('voting/', include("voting.urls")),
+    path('main/', include("main.urls")),
     path('results/', include("results.urls")),    
 ]
