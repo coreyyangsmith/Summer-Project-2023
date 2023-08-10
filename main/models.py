f21189563d4b9d11375255f88314f72590ac74bd
Corey Yang-Smith <yangsmith.corey@hotmail.com>
2023-08-10 08:44:05 -0600

    add question model

--- main/models.py
+++ main/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.gis.db import models
+# from django.contrib.gis.db import models
 import datetime
 from django.utils import timezone
 
@@ -14,14 +14,14 @@ class User(models.Model):
         return self.first_name + " " + self.last_name
 
 class Neighbourhood(models.Model):
-    code = models.CharField(max_length = 3)    
-    name = models.CharField(max_length = 40)
+    code = models.CharField(max_length = 3, default="AAA")    
+    name = models.CharField(max_length = 40, default="DEFAULT")
     image = models.FilePathField(path="/media/uploads", recursive = True)
-    sector = models.CharField(max_length=9)
-    location = models.PointField() # lat long data for neighbourhood geographic 'centre'
-    multipolygon = models.MultiPolygonField() # multipolygon boundary
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
+    sector = models.CharField(max_length=9, default="DEFAULT")
+    # location = models.PointField() # lat long data for neighbourhood geographic 'centre'
+    # multipolygon = models.MultiPolygonField() # multipolygon boundary
+    created_at = models.DateTimeField(default="Created UTC")
+    updated_at = models.DateTimeField(default="Updated UTC")
 
     def __str__(self):
         return self.code + "-" + self.name
@@ -39,26 +39,32 @@ class Voting(models.Model):
         return self.vote_time >= timezone.now() - datetime.timedelta(days=1)
     
 class Metric(models.Model):
-    name = models.CharField(20)
+    name = models.CharField(max_length=20)
     neighourhood_key = models.ForeignKey(Neighbourhood, on_delete = models.CASCADE)
     actual_value = models.FloatField()
     actual_wgt = models.FloatField()
     perceived_value = models.FloatField()
     perceived_wgt = models.FloatField()
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
-
-class Question(models.Model):
-    name = models.CharField(20)
+    created_at = models.DateTimeField(default="Created UTC")
+    updated_at = models.DateTimeField(default="Updated UTC")
 
 class Quiz(models.Model):
     name = models.CharField(max_length=50)
     user_id = models.ForeignKey(User, on_delete = models.CASCADE)
     score = models.PositiveSmallIntegerField()
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
+    created_at = models.DateTimeField(default="Created UTC")
+    updated_at = models.DateTimeField(default="Updated UTC")
+
+class Question(models.Model):
+    name = models.CharField(max_length=20)
+    quiz_id = models.ForeignKey(Quiz, on_delete=models.CASCADE) 
+    comparison = models.CharField(max_length=16) # greaterthan, lessthan, greaterthanequal, lessthanequal, notequal, equal
+    metric_id_1 = models.ForeignKey(Metric, on_delete=models.CASCADE) # grab metric ID to then grab neighbourhood ID for comparison
+    metric_id_2 = models.ForeignKey(Metric, on_delete=models.CASCADE) # grab metric ID to then grab neighbourhood ID for comparison
+    created_at = models.DateTimeField(default="Created UTC")
+    updated_at = models.DateTimeField(default="Updated UTC")
 
 ## Many-to-Many Model Relationship Implementation
 # 
 class QuestionSet(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=20)
