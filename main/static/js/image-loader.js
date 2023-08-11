7f29c6e0ed52f2de0b21748541d35d7d2b22b726
chvaldez10 <christianvaldez021@gmail.com>
2023-08-11 09:46:24 -0600

    added script to get images under community images

--- main/static/js/image-loader.js
+++ main/static/js/image-loader.js
@@ -1,11 +1,27 @@
 var calgaryCommunities = {}; // store community data
 var communityCode = [];
+const imageLeft = $(".image-left");
+const imageRight = $(".image-right");
+const imagePathPrefix = "../../../media/community_images";
 
-$.getJSON("../../../Data/Testing/JSON/communities.json", function (data) {
-  calgaryCommunities = data;
-  communityCode = Object.keys(calgaryCommunities);
-  console.log(communityCode[0]);
-  console.log(communityCode.length);
-}).fail(function (jqXHR, textStatus, errorThrown) {
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function loadRandomImage() {
+  let min = 0;
+  let max = communityCode.length;
+  let randomIntLeft = getRandomInt(min, max);
+  let randomIntRight = getRandomInt(min, max);
+  let randomImageLeft = imagePathPrefix + communityCode[randomIntLeft] + ".jp";
+}
+
+$.getJSON(
+  "../../../media/community_images/utils/communities.json",
+  function (data) {
+    calgaryCommunities = data;
+    communityCode = Object.keys(calgaryCommunities);
+  }
+).fail(function (jqXHR, textStatus, errorThrown) {
   console.error("Error loading JSON data:", textStatus, errorThrown);
 });
