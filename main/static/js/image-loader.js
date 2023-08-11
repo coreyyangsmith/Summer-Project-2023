29216026532bc53c3efb9b6f6c197b72854bf78a
chvaldez10 <christianvaldez021@gmail.com>
2023-08-11 10:01:50 -0600

    added random image

--- main/static/js/image-loader.js
+++ main/static/js/image-loader.js
@@ -1,26 +1,39 @@
 var calgaryCommunities = {}; // store community data
-var communityCode = [];
+var communityPictures = [];
 const imageLeft = $(".image-left");
 const imageRight = $(".image-right");
 const imagePathPrefix = "../../../media/community_images";
 
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function loadRandomImage() {
   let min = 0;
-  let max = communityCode.length;
+  let max = communityPictures.length;
+  console.log(max);
   let randomIntLeft = getRandomInt(min, max);
   let randomIntRight = getRandomInt(min, max);
-  let randomImageLeft = imagePathPrefix + communityCode[randomIntLeft] + ".jp";
+
+  console.log(randomIntLeft);
+
+  // choose images
+  let randomImageLeft =
+    imagePathPrefix + "/" + communityPictures[randomIntLeft];
+  let randomImageRight =
+    imagePathPrefix + "/" + communityPictures[randomIntRight];
+
+  // load image
+  imageLeft.attr("src", randomImageLeft);
+  imageRight.attr("src", randomImageRight);
 }
 
 $.getJSON(
   "../../../media/community_images/utils/communities.json",
   function (data) {
     calgaryCommunities = data;
-    communityCode = Object.keys(calgaryCommunities);
+    communityPictures = Object.values(calgaryCommunities);
+    loadRandomImage();
   }
 ).fail(function (jqXHR, textStatus, errorThrown) {
   console.error("Error loading JSON data:", textStatus, errorThrown);
