86ec2cfc9d49009c129128a1eacbf09ff8def37e
chvaldez10 <christianvaldez021@gmail.com>
2023-08-24 10:03:54 -0600

    added test data to results/views.py

--- results/views.py
+++ results/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# test data for results page and linked Javascript file (delete after)
+TEST_DATA = ["ABB.jpg", "BRI.png", "CIT.png", "DNE.jpg", "FLN.jpg", "KIN.png", "MIS.jpg", "PCK.jpg", "RIV.png", "SHS.png",   "TEM.png"]
+
+data = {"test": TEST_DATA}
+
 def index(response):
-    return render(response, 'results/results.html',{})
+    return render(response, 'results/results.html', data)
