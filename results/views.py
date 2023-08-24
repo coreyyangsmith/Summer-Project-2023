ebbf4939ba19378fadd9709d3a3e1f149ff3a664
chvaldez10 <christianvaldez021@gmail.com>
2023-08-24 11:21:13 -0600

    added javascript for results loader

--- results/views.py
+++ results/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # test data for results page and linked Javascript file (delete after)
-TEST_DATA = ["ABB", "BRI", "CIT", "DNE", "FLN", "KIN", "MIS", "PCK", "RIV", "SHS", "TEM"]
+TEST_DATA = ["ABB", "BRI", "CIT", "DNE", "FLN", "KIN", "MIS", "PCK", "RIV", "SHS"]
 
-test_community_data = {"test": TEST_DATA}
+test_community_data = {"community_codes": TEST_DATA}
 
 def index(response):
     return render(response, "results/results.html", test_community_data)
