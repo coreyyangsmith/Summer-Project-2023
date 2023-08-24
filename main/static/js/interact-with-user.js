04c796d830a86a2eec2f1d18da787922fb8e0406
chvaldez10 <christianvaldez021@gmail.com>
2023-08-24 12:22:35 -0600

    added documentation for interact-with-user.js

--- main/static/js/interact-with-user.js
+++ main/static/js/interact-with-user.js
@@ -1,3 +1,14 @@
+/**
+ * Interacts with user image clicks
+ *
+ * Note (s):
+ *    - Uses the following css classes:
+ *      - image-left, image-right
+ */
+
+/**
+ * Listens for left and right image clicks on home page.
+ */
 $(".image-left").click(function () {
   console.log("image left clicked");
 });
